5d022d8827596e3952c7a63d9de505583e2e61d6
Francesc <franhumbert@gmail.com>
2025-05-13 13:26:05 +0200

    Pruevas

---
+++ Tema2/S1_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$numero = 4;
+$pi = 3.14;
+$hola = "Hola";
+$bool = true;
+const NOM = "Francesc";
+
+echo '<h1>' . NOM . '</h1>';
+echo '<br>' . $numero ;
+echo '<br>' . $pi;
+echo '<br>' . $hola;
+echo '<br>' . $bool;
+?>
+
+</body>
+</html>
