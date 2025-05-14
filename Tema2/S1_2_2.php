841fe8292433b7921b4d7348af876fb4c1376371
Francesc <franhumbert@gmail.com>
2025-05-14 10:30:35 +0200

    Exercici 2 finalitzat

---
+++ Tema2/S1_2_2.php
@@ -0,0 +1,17 @@
+<?php
+
+$txt = "Hello, World!";
+
+echo $txt . "\n"; // Output: Hello, World!
+
+echo(strtoupper($txt). "\n");// Output: en mayusculas.
+
+echo strlen($txt) . "\n";// Output: Longitud.
+
+echo strrev($txt) . "\n";// Output: Invertir cadena.
+
+$txt2 = "Aquest és el curs de PHP";
+
+echo $txt . " " . $txt2;
+
+?>
